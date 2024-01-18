f4b4837fd25e2d4ec80df4f12d8ec692064b7ce5
Daniel Molinero Lucas <57540881+Papishushi@users.noreply.github.com>
2024-01-18 05:14:11 +0100

    Bug fix

--- Batching/Resources/ResourceLoadAttribute.cs
+++ Batching/Resources/ResourceLoadAttribute.cs
@@ -22,8 +22,8 @@ namespace ScapeCore.Core.Batching.Resources
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
     public sealed class ResourceLoadAttribute : Attribute
     {
-        internal readonly Type loadType;
-        internal readonly string[] names;
+        public readonly Type loadType;
+        public readonly string[] names;
 
         public ResourceLoadAttribute(Type loadType, params string[] names)
         {
